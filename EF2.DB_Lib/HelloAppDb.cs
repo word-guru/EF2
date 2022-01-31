f96f2a3605c464a1cced69c4aad5aab1a3d2a1c9
Pavel Popov <legeon48@mail.ru>
2022-01-31 21:53:14 +0300

    Add project files.

---
+++ EF2.DB_Lib/HelloAppDb.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using EF2.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace EF2.DB_Lib
+{
+    public partial class HelloAppDb : DbContext
+    {
+        public HelloAppDb()
+        {
+        }
+
+        public HelloAppDb(DbContextOptions<HelloAppDb> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetConnectionString("connection_to_db.txt"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.FirstName).IsRequired();
+
+                entity.Property(e => e.LastName).IsRequired();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        private static string GetConnectionString(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
