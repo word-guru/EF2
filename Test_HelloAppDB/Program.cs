9967c16a3b00ebd8b09abdf51041f263958618c4
Pavel Popov <legeon48@mail.ru>
2022-02-01 18:49:45 +0300

    Foreve

---
+++ Test_HelloAppDB/Program.cs
@@ -0,0 +1,25 @@
+﻿using EF2.DB_Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test_HelloAppDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           using (HelloAppDb db = new HelloAppDb())
+            {
+                var users = db.Users.ToList();
+
+                Console.WriteLine("Список объектов: ");
+
+                foreach ( var us in users)
+                {
+                    Console.WriteLine($"{us.Id}\t{us.LastName} {us.FirstName}   {us.Age} лет");
+                }
+            }
+        }
+    }
+}
---
+++ dbo.users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[users] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR (MAX) NOT NULL,
+    [LastName]  NVARCHAR (MAX) NOT NULL,
+    [Age]       INT            NOT NULL,
+    CONSTRAINT [PK_users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
